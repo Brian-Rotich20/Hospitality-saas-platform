5b47677ac10f98f633a880ada219561204cb3615
Brian-Rotich20 <ralphrotich@gmail.com>
2026-09-17 23:25:48 +0300

    fixed undefined error

--- backend/src/modules/vendors/vendors.controller.ts
+++ backend/src/modules/vendors/vendors.controller.ts
@@ -2,6 +2,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { VendorService } from './vendors.service.js';
 import { updateVendorSchema, payoutDetailsSchema } from './vendors.schema.js';
+import { VendorFilters } from './vendors.types.js'
 
 const vendorService = new VendorService();
 
@@ -79,11 +80,15 @@ export class VendorController {
   async getAllVendors(request: FastifyRequest, reply: FastifyReply) {
     try {
       const { status, limit, offset } = request.query as { status?: string; limit?: string; offset?: string };
-      const vendors = await vendorService.getAllVendors({
-        status: status as any,
-        limit:  limit  ? parseInt(limit)  : undefined,
-        offset: offset ? parseInt(offset) : undefined,
-      });
+
+      const filters : VendorFilters ={
+        status: status as any
+      }
+       if (limit) filters.limit = parseInt(limit);
+       if (offset) filters.offset = parseInt(offset);
+
+      const vendors = await vendorService.getAllVendors(filters);
+
       return reply.code(200).send({ success: true, data: vendors });
     } catch (error: any) {
       return reply.code(400).send({ success: false, error: error.message });
